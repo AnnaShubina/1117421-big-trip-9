7557299fdf42620996ee560baeff277446d28580
AnnaChernova <ashubina0810@mail.ru>
2019-08-17 11:02:53 +0300

    сформирует данные информации о маршруте

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,8 +1,14 @@
+const getCitiesCount = (cards) => {
+  const cities = cards.map(({city}) => city);
+  const uniqCity = new Set(cities);
+  return uniqCity.size;
+}
 export const getTripInfoTemplate = (cards) => {
   return `
-    <div class="trip-info__main">
+    <div class="trip-info__main"> 
+      ${getCitiesCount(cards) > 2 ? `
       <h1 class="trip-info__title">${cards[0].city} &mdash; ... &mdash; ${cards[cards.length - 1].city}</h1>
+      ` : ` <h1 class="trip-info__title">${cards[0].city} &mdash; ${cards[cards.length - 1].city}</h1>`} 
       <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
-    </div>
-  `.trim();
+    </div>`.trim();
 };
